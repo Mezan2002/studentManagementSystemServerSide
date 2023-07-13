ac70be2515d1cd946eb32bc17785cc7871dd3357
Mezan2002 <mezanurr009@gmail.com>
2023-07-13 17:48:58 +0600

    User register api done and its working

--- index.js
+++ index.js
@@ -7,6 +7,53 @@ const app = express();
 const port = process.env.PORT || 3000;
 // * require end
 
+// * middlewares start
+
+app.use(cors());
+app.use(express.json());
+
+// * middlewares end
+
+// * connect mongoDB start
+const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+// * connect mongoDB end
+
+// * collections start
+
+const usersCollection = client.db("studentManagersDBUser").collection("users");
+
+// * collections end
+
+// * CRUD run function start
+const run = async () => {
+  try {
+    // * register user API start
+
+    app.post("/registerUser", async (req, res) => {
+      const usersData = req.body;
+      console.log(usersData);
+      const result = await usersCollection.insertOne(usersData);
+      res.send(result);
+    });
+
+    // * register user API end
+  } finally {
+    console.log();
+  }
+};
+
+run().catch((err) => console.log(err));
+// * CRUD run function end
+
 // * initial configurations for express start
 app.get("/", (req, res) => {
   res.send("Student Management Server is Running Hurrah!");
